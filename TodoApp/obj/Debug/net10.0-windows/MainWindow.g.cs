f8b71a05659465c2592bc34df587fadd28ce3ffc
user1 <user1@thehpc.in>
2026-01-18 19:14:03 +0530

    updated 2

--- TodoApp/obj/Debug/net10.0-windows/MainWindow.g.cs
+++ TodoApp/obj/Debug/net10.0-windows/MainWindow.g.cs
@@ -52,7 +52,7 @@ namespace TodoApp {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/TodoApp;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/TodoApp;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
